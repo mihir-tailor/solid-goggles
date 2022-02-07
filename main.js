cf53667634bf1afca1bea0e64716380e19dd6a5f
Milind Bhakta <desire2dosomething@gmail.com>
2022-02-07 01:18:52 -0800

    added logic for mergesort

--- main.js
+++ main.js
@@ -36,3 +36,40 @@ function BubbleSort() {
     });
 }
 
+function mSort() {
+   let result =  mergeSort(elm);
+   console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+   console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Merge sort ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+   result.forEach(element => {
+       console.log(element);
+   });
+}
+
+function mergeSort(arr) {
+    const half = arr.length / 2;
+
+    // the base case is array length <=1
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const left = arr.splice(0, half); // the first half of the array
+    const right = arr;
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let sortedArr = []; // the sorted elements will go here
+
+    while (left.length && right.length) {
+        // insert the smallest element to the sortedArr
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+
+    // use spread operator and create a new array, combining the three arrays
+    return [...sortedArr, ...left, ...right];
+}
